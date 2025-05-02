e83bed5f231c741e5f36f14925ea42e46f25ba63
JhairCortezUcb <jhair.cortez@ucb.edu.bo>
2025-05-02 17:11:41 -0400

    Update Ejercicio_01.cpp

--- Ejercicio_01.cpp
+++ Ejercicio_01.cpp
@@ -1,12 +1,30 @@
 #include <iostream>
-#include <iomanip>
 using namespace std;
 
-int main() 
+void convertirAHexadecimal(int numero)
+{
+    char hex[20];
+    int i = 0;
+    while (numero > 0)
+        int resto = numero % 16;
+        if (resto < 10)
+            hex[i] = resto + '0';
+        else
+            hex[i] = resto - 10 + 'A';
+        i++;
+        numero = numero / 16;
+    
+    cout << "Hexadecimal: ";
+    for (int j = i - 1; j >= 0; j--)
+        cout << hex[j];
+    cout << endl;
+}
+
+int main()
 {
     int n;
     cout << "Ingrese un número: ";
     cin >> n;
-    cout << "El número en hexadecimal es: " << hex << uppercase << n << endl;
+    convertirAHexadecimal(n);
     return 0;
 }
